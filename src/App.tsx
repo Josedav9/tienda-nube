e89fa3264f92330e3b3763ef82a185b98dd07a7b
Irlanda Jeanete Castillo Vega <icastillov1998@gmail.com>
2022-10-10 23:11:28 -0500

    feat: the navigation configuration was added in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { ToastContainer } from "react-toastify";
+import Navigation from "./presentation/routes/Navigation";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test plugin for tienda nube</h1>
-      <div className="container">
-        <h2>
-          List of orders
-          <li></li>
-        </h2>
-      </div>
-    </div>
+    <>
+      <Navigation />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 }
 
