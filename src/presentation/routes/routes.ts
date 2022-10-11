cb83766441f09f400ffe0dd597506a5e85691fba
Irlanda Jeanete Castillo Vega <icastillov1998@gmail.com>
2022-10-10 23:08:17 -0500

    feat: the current routes were added with the own layout

---
+++ src/presentation/routes/routes.ts
@@ -0,0 +1,20 @@
+//Load pages
+import Store from "../pages/pages/Store";
+//Layouts
+import LayoutMenu from "../layouts/LayoutMenu";
+
+const routes = [
+  {
+    path: "/",
+    layout: LayoutMenu,
+    component: Store,
+  },
+  {
+    //404
+    path: "/*",
+    layout: LayoutMenu,
+    component: Store,
+  },
+];
+
+export default routes;
